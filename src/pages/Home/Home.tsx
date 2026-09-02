669416a199ae33c60e0afebea1b0d46be2350093
Josh Grajam Falcón Cabra <jgcarwayfc@gmail.com>
2026-09-02 18:21:24 -0500

    creado mapa y colocado en el home

---
+++ src/pages/Home/Home.tsx
@@ -0,0 +1,11 @@
+import './Home.css'
+import Map from "../../components/Map/Map"
+
+function Home() {
+    return (
+        <main className="home">
+            <Map />
+        </main>
+    )
+}
+export default Home
